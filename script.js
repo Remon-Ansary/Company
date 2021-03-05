e0abc9fd102ec67ea98351d209a4cad9b0c255c0
Remon Ansary <remonansary@gmail.com>
2021-03-05 14:34:38 +0600

    active,search,popup

--- script.js
+++ script.js
@@ -96,3 +96,46 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
+
+
+
+
+//  this is for search page popup
+$(document).ready(function() {
+
+	//success toast
+	
+	var options = {
+	autoClose: true,
+	progressBar: true,
+	enableSounds: true,
+	sounds: {
+	
+	info: "https://res.cloudinary.com/dxfq3iotg/video/upload/v1557233294/info.mp3",
+	// path to sound for successfull message:
+	success: "https://res.cloudinary.com/dxfq3iotg/video/upload/v1557233524/success.mp3",
+	// path to sound for warn message:
+	warning: "https://res.cloudinary.com/dxfq3iotg/video/upload/v1557233563/warning.mp3",
+	// path to sound for error message:
+	error: "https://res.cloudinary.com/dxfq3iotg/video/upload/v1557233574/error.mp3",
+	},
+	};
+	
+	var toast = new Toasty(options);
+	toast.configure(options);
+	
+	
+	
+	
+	
+	$('#successtoast').click(function() {
+	
+	toast.success("This toast notification with sound");
+	
+	});
+	
+	
+	
+	});
+
+
